03f2e5e9255152981dd607366f968a1aa186057c
Nikola Obradovic <obradovicnik01@gmail.com>
2019-08-21 16:39:37 +0200

    First commit

---
+++ src/js/models/Search.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+  async getResults() {
+    const key = 'b748d90404dd61ba205ae975ee7a2d66'
+    try {
+      const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`)
+      this.result = res.data.recipes;
+      // console.log(this.result)
+    } catch (err) {
+      alert(err)
+    }
+  }
+}
