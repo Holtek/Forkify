9d3b105be154b9cc108604491a5b0ea1f3dfb552
Nikola Obradovic <obradovicnik01@gmail.com>
2019-08-21 17:57:16 +0200

    Added Search Results and Loading Spinner

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,10 @@
-import Search from "./models/Search"
-import * as searchView from "./views/searchView"
+import Search from './models/Search'
+import * as searchView from './views/searchView'
 import {
-  elements
-} from "./views/base"
+  elements,
+  renderLoader,
+  clearLoader
+} from './views/base';
 // Global state of the app
 // -Search object
 // -Current recipe object
@@ -11,19 +13,21 @@ const state = {}
 
 const controlSearch = async () => {
   // 1. Get the query from view
-  const query = searchView.getInput
-  console.log(query);
+  const query = searchView.getInput()
   if (query) {
     // 2. New search object and add it to the state
     state.search = new Search(query)
 
     // 3.Prepare UI for results
-
+    searchView.clearInput();
+    searchView.clearResults();
+    renderLoader(elements.searchRes)
     // 4.Search for recipes
     await state.search.getResults();
 
     // 5. Render results on UI
-    console.log(state.search.result)
+    clearLoader();
+    searchView.renderResults(state.search.result)
   }
 }
 document.querySelector('.search').addEventListener('submit', e => {
